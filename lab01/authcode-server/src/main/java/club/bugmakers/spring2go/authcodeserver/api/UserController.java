3cf331d03c2e771ef5358a667f14a1bd19ae4360
bruce.ouyang <bruce.ouyang@bugmakers.club>
2020-03-12 01:22:46 +0800

    add 基于 OAuth2 的授权服务器

---
+++ lab01/authcode-server/src/main/java/club/bugmakers/spring2go/authcodeserver/api/UserController.java
@@ -0,0 +1,25 @@
+package club.bugmakers.spring2go.authcodeserver.api;
+
+import club.bugmakers.spring2go.authcodeserver.model.UserInfo;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/api/userinfo")
+    public ResponseEntity<UserInfo> getUserInfo() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        User user = (User) authentication.getPrincipal();
+
+        String email = user.getUsername() + "@bugmakers.club";
+
+        return ResponseEntity.ok(UserInfo.builder().name(user.getUsername()).email(email).build());
+    }
+}
---
+++ lab01/authcode-server/src/main/java/club/bugmakers/spring2go/authcodeserver/model/UserInfo.java
@@ -0,0 +1,15 @@
+package club.bugmakers.spring2go.authcodeserver.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserInfo {
+    private String name;
+    private String email;
+}
